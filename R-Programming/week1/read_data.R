97ce36f2ccb820cacfa8e60633be2c909242d9fd
connectedblue <connectedblue@users.noreply.github.com>
2016-01-09 16:46:07 +0000

    finished week one quiz

---
+++ R-Programming/week1/read_data.R
@@ -0,0 +1,8 @@
+test_data<-read.table(file="C:\\Users\\cs\\Dropbox\\Connected Blue\\Training\\data science\\git\\datascience\\R-Programming\\week1\\bristol_spend.csv", sep=",",header=TRUE, nrows=21)
+
+coltypes=sapply(test_data, class)
+
+all_data<-read.table(file="C:\\Users\\cs\\Dropbox\\Connected Blue\\Training\\data science\\git\\datascience\\R-Programming\\week1\\bristol_spend.csv", colClasses=coltypes, sep=",",header=TRUE, fill=TRUE)
+
+
+
