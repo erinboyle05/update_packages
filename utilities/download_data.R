a53b756ef98f9bb1c26cc90d93db72bea75bc27b
connectedblue <connectedblue@users.noreply.github.com>
2016-02-15 11:30:58 +0000

    Added some comments and cleaned up code

--- utilities/download_data.R
+++ utilities/download_data.R
@@ -6,17 +6,24 @@
 # datasetname - the name of the dataset.  This could be different to filename,
 #               for example an unzipped file might have a different name to the download
 # zipdelete - if true, then a downloaded zip is deleted after sucessfully unzipping
+# gitignore - if true, then the downloaded files will be added to .gitignore for the 
+#             current directory so they won't get included in the commits
 
 library(tools)
 
-download_dataset<-function(url, filename="data", datasetname="", zipdelete=TRUE) {
+download_dataset<-function(url, filename="data", datasetname="",
+                           zipdelete=TRUE, gitignore=TRUE) {
 
   # Don't do anything if the dataset is already present in the working directory
   if ((file.exists(filename) | file.exists(datasetname))){
       return()
   } 
   zipfilecontents<-""
+  
+  # get the file and save it
   download.file(url = url, destfile = filename)
+  
+  # for zip files, attempt to extract and delete the original zip
   if (file_ext(filename)=="zip") {
       zipfilecontents<-unzip(filename, list=TRUE)[["Name"]]    
       unzipfile <- tryCatch(
@@ -28,14 +35,19 @@ download_dataset<-function(url, filename="data", datasetname="", zipdelete=TRUE)
                 unlink(filename)
       }
   }
-  add_to_gitignore(c(filename, zipfilecontents))
+  
+  # add downloaded datafile names to gitignore
+  if(gitignore)  add_to_gitignore(c(filename, zipfilecontents))
 }
 
 add_to_gitignore<-function(new_files_to_ignore){
         gitignore<-".gitignore"
         ensure_file_exists(gitignore)
+        # get current contents
         current_files_to_ignore<-readLines(gitignore)
+        # make new contents, eliminating any duplicates
         new_contents<-unique(c(new_files_to_ignore, current_files_to_ignore))
+        # write new gitignore file
         writeLines(new_contents, gitignore)
 }
 
