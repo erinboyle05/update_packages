ee2abe9375a9729bae92a335e182c5f1e0fc65a2
connectedblue <connectedblue@users.noreply.github.com>
2016-02-15 11:17:06 +0000

    added support for gitignore when downloading data

--- utilities/download_data.R
+++ utilities/download_data.R
@@ -15,16 +15,33 @@ download_dataset<-function(url, filename="data", datasetname="", zipdelete=TRUE)
   if ((file.exists(filename) | file.exists(datasetname))){
       return()
   } 
-
+  zipfilecontents<-""
   download.file(url = url, destfile = filename)
   if (file_ext(filename)=="zip") {
-    unzipfile <- tryCatch(
-      unzip(filename),
-      error=function(e) e
-    )
-    # remove the zip file if there are no unzip errors
-    if(!inherits(unzipfile, "error")) {
-      unlink(filename)
-    }
+      zipfilecontents<-unzip(filename, list=TRUE)[["Name"]]    
+      unzipfile <- tryCatch(
+                unzip(filename),
+                warning=function(e) e
+                )
+      # remove the zip file if there are no unzip warnings and zipdelete is TRUE
+      if(!inherits(unzipfile, "warning") & zipdelete) {
+                unlink(filename)
+      }
   }
+  add_to_gitignore(c(filename, zipfilecontents))
+}
+
+add_to_gitignore<-function(new_files_to_ignore){
+        gitignore<-".gitignore"
+        ensure_file_exists(gitignore)
+        current_files_to_ignore<-readLines(gitignore)
+        new_contents<-unique(c(new_files_to_ignore, current_files_to_ignore))
+        writeLines(new_contents, gitignore)
+}
+
+ensure_file_exists <-function(file){
+        # if a filename doesn't exist, create a zero byte one
+        if(!file.exists(file)){
+                write.table(data.frame(), file=file, col.names=FALSE)
+        }
 }
